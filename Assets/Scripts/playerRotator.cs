7c66e931278e6a575d2d7c3feea622bc178ab2a2
Erik Torres <etorres2907@sdsu.edu>
2021-12-05 21:37:50 -0800

    Added sound to rotating shape, modified scripts to play sound

--- Assets/Scripts/playerRotator.cs
+++ Assets/Scripts/playerRotator.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class playerRotator : MonoBehaviour
 {
     [SerializeField] float xAngle, yAngle, zAngle;
-
+    public AudioClip shuffleSound;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             // Rotates the model
             transform.Rotate(xAngle, yAngle, zAngle, Space.World);
+            AudioSource.PlayClipAtPoint(shuffleSound, transform.position);
         }
     }
 }
